ccd4cb3881c2c089c20121bb7c7b2fa64e7e18a7
Zeaur Rahman <rahmanzea31@gmail.com>
2024-10-27 20:46:12 +0600

    update

--- api/handler.py
+++ api/handler.py
@@ -1,6 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from django.http import JsonResponse
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WorkWonders.settings")
 application = get_wsgi_application()
@@ -10,21 +9,32 @@ def handler(event, context):
         "REQUEST_METHOD": event['httpMethod'],
         "PATH_INFO": event['path'],
         "QUERY_STRING": event['queryStringParameters'] or '',
-        "wsgi.input": event['body'] or '',
+        "wsgi.input": event['body'].encode() if event['body'] else b'',  # Encode body as bytes
         "wsgi.url_scheme": "https" if event['headers'].get('X-Forwarded-Proto', '').lower() == 'https' else 'http',
         "CONTENT_TYPE": event['headers'].get('Content-Type', ''),
         "CONTENT_LENGTH": str(len(event['body'])) if event['body'] else '0',
     }
 
-    # Add other headers if necessary
+   
+    headers = []
+
+   
     for key, value in event['headers'].items():
         environ[f'HTTP_{key.upper().replace("-", "_")}'] = value
 
-    status, headers = application(environ, lambda status, headers: (status, headers))
+   
+    status_code = '200 OK'
+    
+    def start_response(status, response_headers):
+        nonlocal status_code
+        status_code = status
+        headers[:] = response_headers  
 
-    response_body = JsonResponse(status, safe=False).content
+    response_body = application(environ, start_response)
+
+    
     return {
-        "statusCode": status,
-        "headers": {header[0]: header[1] for header in headers},
-        "body": response_body.decode(),
+        "statusCode": int(status_code.split(' ')[0]),  
+        "headers": {header[0]: header[1] for header in headers},  
+        "body": b''.join(response_body).decode(),  
     }
